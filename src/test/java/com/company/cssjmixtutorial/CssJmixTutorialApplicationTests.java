afaad417784b834f1e06abdd0ddc03691c363356
Chong <99638559+puchongdev@users.noreply.github.com>
2022-10-28 09:01:54 +0700

    install jmix

---
+++ src/test/java/com/company/cssjmixtutorial/CssJmixTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cssjmixtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssJmixTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
